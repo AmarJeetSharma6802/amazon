9c0b6d99936b44df8841b10b91fe25b243156e38
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-05-08 10:46:13 +0530

    Refactor Homepage component to implement two carousels with improved scrolling functionality and add watch items

--- app/home/Homepage.jsx
+++ app/home/Homepage.jsx
@@ -1,96 +1,138 @@
 "use client"
 import Image from 'next/image'
-import React, { useEffect, useState,useRef } from 'react'
-
+import React, { useEffect, useState, useRef } from 'react'
 
 function Homepage() {
-  const [data,setData] = useState([])
+  const [data, setData] = useState([])
 
-  useEffect(()=>{
-    const fetchData = async()=>{
+  const watchItem = [
+    { id: 1, img: "https://m.media-amazon.com/images/I/81oS2scJ5GL._AC_SY200_.jpg" },
+    { id: 2, img: "https://m.media-amazon.com/images/I/61-vRq2ulOL._AC_SY200_.jpg" },
+    { id: 3, img: "https://m.media-amazon.com/images/I/61bm7UEdGAL._AC_SY200_.jpg" },
+    { id: 4, img: "https://m.media-amazon.com/images/I/61QiBo-sPTL._AC_SY200_.jpg" },
+    { id: 5, img: "https://m.media-amazon.com/images/I/610BF3kqBZL._AC_SY200_.jpg" },
+    { id: 6, img: "https://m.media-amazon.com/images/I/61ATaTpvEQL._AC_SY200_.jpg" },
+    { id: 7, img: "https://m.media-amazon.com/images/I/61akt30bJsL._AC_SY200_.jpg" },
+    { id: 8, img: "https://m.media-amazon.com/images/I/61q0ZgCYoJL._AC_SY200_.jpg" },
+    { id: 9, img: "https://m.media-amazon.com/images/I/61akt30bJsL._AC_SY200_.jpg" },
+    { id: 10, img: "https://m.media-amazon.com/images/I/719NDsHC59L._AC_SY200_.jpg" },
+    { id: 11, img: "https://m.media-amazon.com/images/I/71ICbdiFLxL._AC_SY200_.jpg" },
+    { id: 12, img: "https://m.media-amazon.com/images/I/71SuFsBkLgL._AC_SY200_.jpg" },
+    { id: 13, img: "https://m.media-amazon.com/images/I/71FFcbkVcML._AC_SY200_.jpg" },
+    { id: 14, img: "https://m.media-amazon.com/images/I/71ejV7TaDfL._AC_SY200_.jpg" },
+  ]
+
+  useEffect(() => {
+    const fetchData = async () => {
       try {
-        // const res = await fetch("/api/itemStore",{
-        const res = await fetch("https://amazon-tau-two.vercel.app/api/itemStore",{
-          method:"GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
+        const res = await fetch("/api/itemStore", {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
           cache: "no-store",
         })
         const data = await res.json()
-        setData(data) 
-        console.log("data", data)
+        setData(data)
       } catch (error) {
         console.log(error)
       }
     }
     fetchData()
-  },[])
+  }, [])
 
-  const carouselRef = useRef(null);
-  
-    const [canScrollLeft, setCanScrollLeft] = useState(false);
-    const [canScrollRight, setCanScrollRight] = useState(true);
-  
-    // Scroll carousel function
-    const scrollCarousel = (direction) => {
-      const carousel = carouselRef.current;
-      const scrollAmount = 300;
-  
-      if (direction === "left") {
-        carousel.scrollBy({
-          left: -scrollAmount,
-          behavior: "smooth",
-        });
-      } else {
-        carousel.scrollBy({
-          left: scrollAmount,
-          behavior: "smooth",
-        });
-      }
-    };
+  // Carousel 1 refs and states
+  const carouselRef1 = useRef(null)
+  const [canScrollLeft1, setCanScrollLeft1] = useState(false)
+  const [canScrollRight1, setCanScrollRight1] = useState(true)
 
-    const handleScroll = () => {
-      const carousel = carouselRef.current;
-      const maxScrollLeft = carousel.scrollWidth - carousel.clientWidth;
-  
-      // Disable left button when at the start
-      setCanScrollLeft(carousel.scrollLeft > 0);
-  
-      // Disable right button when at the end
-      setCanScrollRight(carousel.scrollLeft < maxScrollLeft);
-    };
-  
+  // Carousel 2 refs and states
+  const carouselRef2 = useRef(null)
+  const [canScrollLeft2, setCanScrollLeft2] = useState(false)
+  const [canScrollRight2, setCanScrollRight2] = useState(true)
+
+  const scrollCarousel = (ref, direction) => {
+    const carousel = ref.current
+    const scrollAmount = 300
+
+    carousel.scrollBy({
+      left: direction === "left" ? -scrollAmount : scrollAmount,
+      behavior: "smooth",
+    })
+  }
+
+  const handleScroll = (ref, setLeft, setRight) => {
+    const carousel = ref.current
+    const maxScrollLeft = carousel.scrollWidth - carousel.clientWidth
+    setLeft(carousel.scrollLeft > 0)
+    setRight(carousel.scrollLeft < maxScrollLeft)
+  }
+
   return (
     <div>
-         <div className="carousel-container-2 caroucel_container_bg">
-                  <button
-                    className="scroll-button-2 left-2"
-                    onClick={() => scrollCarousel("left")}
-                    disabled={!canScrollLeft}
-                  >
-                    <Image src="/left-arrow.png" alt="" id="left-img" width={25} height={25}  />
-                  </button>
-                  <div className="carousel-2" ref={carouselRef} onScroll={handleScroll}>
-                    {data.map((item, index) => (
-                      <div key={item._id || index}  className='carousel-item-2 carousel-item-fetch'>
-                      <img src={item.image} alt={item.title || "Image"} className="carousel-image-2" />
-                    </div>
-                    
-                    ))}
-                  </div>
-                  <button
-                    className="scroll-button-2 right-2"
-                    onClick={() => scrollCarousel("right")}
-                    disabled={!canScrollRight}
-                  >
-                     <Image src="/right-arrow.png" alt="" id="left-img" width={25} height={25}  />
-                  </button>
-          </div>
+      {/* Carousel 1 */}
+      <div className="carousel-container-2 caroucel_container_bg">
+        <button
+          className="scroll-button-2 left-2"
+          onClick={() => scrollCarousel(carouselRef1, "left")}
+          disabled={!canScrollLeft1}
+        >
+          <Image src="/left-arrow.png" alt="" width={25} height={25} />
+        </button>
+        <div
+          className="carousel-2"
+          ref={carouselRef1}
+          onScroll={() => handleScroll(carouselRef1, setCanScrollLeft1, setCanScrollRight1)}
+        >
+          {data.map((item, index) => (
+            <div key={item._id || index} className="carousel-item-2 carousel-item-fetch">
+              <img src={item.image} alt={item.title || "Image"} className="carousel-image-2" />
+            </div>
+          ))}
+        </div>
+        <button
+          className="scroll-button-2 right-2"
+          onClick={() => scrollCarousel(carouselRef1, "right")}
+          disabled={!canScrollRight1}
+        >
+          <Image src="/right-arrow.png" alt="" width={25} height={25} />
+        </button>
+      </div>
+
+      {/* Carousel 2 */}
+      <div className="carousel-container-2 caroucel_container_bg">
+        <button
+          className="scroll-button-2 left-2"
+          onClick={() => scrollCarousel(carouselRef2, "left")}
+          disabled={!canScrollLeft2}
+        >
+          <Image src="/left-arrow.png" alt="" width={25} height={25} />
+        </button>
+        <div
+          className="carousel-2"
+          ref={carouselRef2}
+          onScroll={() => handleScroll(carouselRef2, setCanScrollLeft2, setCanScrollRight2)}
+        >
+          {watchItem.map((item, index) => (
+            <div key={item.id} className="carousel-item-2 carousel-item-fetch watch_item_size">
+              <Image
+                src={item.img}
+                alt={item.title || "Image"}
+                width={100}
+                height={200}
+                className="carousel-image-2"
+              />
+            </div>
+          ))}
+        </div>
+        <button
+          className="scroll-button-2 right-2"
+          onClick={() => scrollCarousel(carouselRef2, "right")}
+          disabled={!canScrollRight2}
+        >
+          <Image src="/right-arrow.png" alt="" width={25} height={25} />
+        </button>
+      </div>
     </div>
   )
 }
 
 export default Homepage
-
-
