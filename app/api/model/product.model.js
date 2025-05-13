8ef93a20c857c2be541c82822f5ad53d2b71531d
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-05-13 10:48:39 +0530

    Refactor product model schema for improved data types and consistency

--- app/api/model/product.model.js
+++ app/api/model/product.model.js
@@ -2,19 +2,39 @@ import mongoose from "mongoose";
 
 const ProductSchema = new mongoose.Schema(
   {
-    details: { type: String },
-    price: { type: Number },
-    rating: { type: String },
-    totalReviews: { type: String },
-    offer: { type: String },
-    image: { type: String },
+    details: {
+      type: String,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      min: 0,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+      min: 0,
+      max: 5,
+    },
+    totalReviews: {
+      type: Number,
+      min: 0,
+    },
+    offer: {
+      type: String,
+      default: "",
+    },
+    image: {
+      type: String,
+      trim: true,
+    },
   },
   {
     timestamps: true,
   }
-)
+);
 
-const productItems =
-  mongoose.models.productItems || mongoose.model("productItems", ProductSchema);
+const Product =
+  mongoose.models.Product || mongoose.model("Product", ProductSchema);
 
-export default productItems;
+export default Product;
