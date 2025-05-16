5c6c746a3269f06557c74f528accbbc40577d83c
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-05-16 18:24:41 +0530

    Refactor InnerItemsNav component for improved mobile navigation and styling

--- app/innerItems/ac/InnerItemsNav.jsx
+++ app/innerItems/ac/InnerItemsNav.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React,{useState} from "react";
+import React,{useRef, useState} from "react";
 import style from "../../style/innerItems.module.css";
 import Image from "next/image";
 
@@ -7,16 +7,46 @@ function InnerItemsNav() {
   const [isHoverKitch, setIsHoverKitch] = useState(false);
   const [isHoverFurniture, setIsHoverFurniture] = useState(false);
   const [isHover, setIsHover] = useState(false);
+  const [nav ,setNav] = useState(false)
+
+//   const ref =useRef ({
+// Kitchen :null,
+// HomeFurnishing:null,
+// Furniture:null
+//   })
+
+  // const handleDrop=(section)=>{
+  //   ref.current[section]?.classList.toggle('show');
+  // }
+
+
+  const handleNav =()=>{
+    setNav(prev => !prev)
+  }
+  
 
   return (
     <>
-      <div className={style.amazon_items}>
+    <div className={style.mobile_view_amazon_items_filters}>
+      <div className={style.mobile_view_amazon_items_details} onClick={handleNav} >
+        <span><i className="fa-solid fa-bars"></i></span>
+        <span>Amazon Home </span>
+      </div>
+      <div className={style.mobile_view_amazon_items_filters_details} >
+        <span>Filter </span>
+        <span><i className="fa-solid fa-sliders"></i></span>
+      </div>
+    </div>
+      <div className={`${style.amazon_items} ${nav ? style.active:""}`}>
         <ul className={style.amazon_items_ul}>
-           <li className={style.amazon_items_li}>Amazon Home</li>
+           <li className={style.amazon_items_li}><div className={style.felx_arrow}>
+            Amazon Home <span className={style.mobile_view_down_arrow}><i className="fa-solid fa-angle-down"></i></span>
+            </div></li>
           <li className={`${style.amazon_items_li} ${style.amazon_items_li_hover}`}
            onMouseEnter={() => setIsHoverKitch (true)}
-           
-          > Kitchen & Home Appliances
+            // onClick={() => handleDrop("Kitchen")}
+            // ref={(el) => (ref.current.Kitchen = el)}
+          ><div className={style.felx_arrow}> Kitchen & Home Appliances <span className={style.mobile_view_down_arrow}><i className="fa-solid fa-angle-down"></i></span></div>
           { 
             isHoverKitch && (
                 <div className={style.inner_div}
@@ -74,9 +104,9 @@ function InnerItemsNav() {
 
               <div className={style.inner_div_right}>
               <div className={style.inner_div_child}>
-                <Image src="https://m.media-amazon.com/images/G/31/img16/Home-SA/1049311_recipe_380x500_1._V509410474_.jpg" alt="image" width={200} height={300} />
-                <Image src="https://m.media-amazon.com/images/G/31/img16/Home-SA/1049311_fans_380x500_1._V509410474_.jpg" alt="image" width={200} height={300} />
-                <Image src="https://m.media-amazon.com/images/G/31/img16/Home-SA/1049311_coolers_380x500._V509410410_.jpg" alt="image" width={200} height={300} />
+                <Image src="https://m.media-amazon.com/images/G/31/img16/Home-SA/1049311_recipe_380x500_1._V509410474_.jpg" alt="image" width={200} height={300} className={style.mobile_view_width_img} />
+                <Image src="https://m.media-amazon.com/images/G/31/img16/Home-SA/1049311_fans_380x500_1._V509410474_.jpg" alt="image" width={200} height={300} className={style.mobile_view_width_img}  />
+                <Image src="https://m.media-amazon.com/images/G/31/img16/Home-SA/1049311_coolers_380x500._V509410410_.jpg" alt="image" width={200} height={300} className={style.mobile_view_width_img} />
               </div>
               </div>
 
@@ -84,9 +114,10 @@ function InnerItemsNav() {
             )
           }
           </li>
-          <li className={style.amazon_items_li}> Large Appliances</li>
-          <li className={style.amazon_items_li}>Kitchen & Dining</li>
-          <li className={style.amazon_items_li} onMouseEnter={() => setIsHoverFurniture (true)} >Furniture
+          <li className={style.amazon_items_li}> <div className={style.felx_arrow}>
+            Large Appliances <span className={style.mobile_view_down_arrow}><i className="fa-solid fa-angle-down"></i></span></div></li>
+          <li className={style.amazon_items_li}><div className={style.felx_arrow}>Kitchen & Dining <span className={style.mobile_view_down_arrow}><i className="fa-solid fa-angle-down"></i></span></div></li>
+          <li className={style.amazon_items_li}onClick={()=>handleDrop("Furniture")} onMouseEnter={() => setIsHoverFurniture (true)} ><div className={style.felx_arrow}>Furniture <span className={style.mobile_view_down_arrow}><i className="fa-solid fa-angle-down"></i></span></div>
             { 
             isHoverFurniture && (
                 <div className={style.inner_div}
@@ -144,9 +175,9 @@ function InnerItemsNav() {
 
               <div className={style.inner_div_right}>
               <div className={style.inner_div_child}>
-                <Image src="https://m.media-amazon.com/images/G/31/img16/furniture/megamenu/sofa._V507978361_.jpg" alt="image" width={200} height={300} />
-                <Image src="https://m.media-amazon.com/images/G/31/img16/furniture/megamenu/SolidWood._V507978361_.jpg" alt="image" width={200} height={300} />
-                <Image src="https://m.media-amazon.com/images/G/31/img16/furniture/megamenu/wing-chair-3._V507978360_.jpg" alt="image" width={200} height={300} />
+                <Image src="https://m.media-amazon.com/images/G/31/img16/furniture/megamenu/sofa._V507978361_.jpg" alt="image" width={200} height={300} className={style.mobile_view_width_img}  />
+                <Image src="https://m.media-amazon.com/images/G/31/img16/furniture/megamenu/SolidWood._V507978361_.jpg" alt="image" width={200} height={300} className={style.mobile_view_width_img}  />
+                <Image src="https://m.media-amazon.com/images/G/31/img16/furniture/megamenu/wing-chair-3._V507978360_.jpg" alt="image" width={200} height={300} className={style.mobile_view_width_img}  />
               </div>
               </div>
 
@@ -154,7 +185,7 @@ function InnerItemsNav() {
             )
           }
           </li>
-          <li className={style.amazon_items_li} onMouseEnter={() => setIsHover(true)}>Home Furnishing
+          <li className={style.amazon_items_li} onClick={()=>handleDrop("HomeFurnishing")} onMouseEnter={() => setIsHover(true)}> <div className={style.felx_arrow}>Home Furnishing <span className={style.mobile_view_down_arrow}><i className="fa-solid fa-angle-down"></i></span></div>
             { 
             isHover && (
                 <div className={style.inner_div}
@@ -212,9 +243,9 @@ function InnerItemsNav() {
 
               <div className={style.inner_div_right}>
               <div className={style.inner_div_child}>
-                <Image src="https://m.media-amazon.com/images/G/31/img17/Home/1049382-HomeSN-380x500-Bedsheets._V507978050_.jpg" alt="image" width={200} height={300} />
-                <Image src="https://m.media-amazon.com/images/G/31/img17/Home/1049382-HomeSN-380x500-Cushions_CushionCovers._V507978050_.jpg" alt="image" width={200} height={300} />
-                <Image src="https://m.media-amazon.com/images/G/31/IMG20/61hXW_RrbgL_SL1200._CB1587025064_.jpg" alt="image" width={200} height={300} />
+                <Image src="https://m.media-amazon.com/images/G/31/img17/Home/1049382-HomeSN-380x500-Bedsheets._V507978050_.jpg" alt="image" width={200} height={300} className={style.mobile_view_width_img}  />
+                <Image src="https://m.media-amazon.com/images/G/31/img17/Home/1049382-HomeSN-380x500-Cushions_CushionCovers._V507978050_.jpg" alt="image" width={200} height={300} className={style.mobile_view_width_img}  />
+                <Image src="https://m.media-amazon.com/images/G/31/IMG20/61hXW_RrbgL_SL1200._CB1587025064_.jpg" alt="image" width={200} height={300} className={style.mobile_view_width_img}  />
               </div>
               </div>
 
@@ -222,10 +253,11 @@ function InnerItemsNav() {
             )
           }
           </li>
-          <li className={style.amazon_items_li}>Home Decor</li>
-          <li className={style.amazon_items_li}>Home Improvement</li>
-          <li className={style.amazon_items_li}>Garden & Outdoor</li>
-          <li className={style.amazon_items_li}>Storage & Organisation</li>
+          <li className={style.amazon_items_li}> <div className={style.felx_arrow}>
+            Home Decor <span className={style.mobile_view_down_arrow}><i className="fa-solid fa-angle-down"></i></span></div></li>
+          <li className={style.amazon_items_li}><div className={style.felx_arrow}>Home Improvement <span className={style.mobile_view_down_arrow}><i className="fa-solid fa-angle-down"></i></span></div></li>
+          <li className={style.amazon_items_li}><div className={style.felx_arrow}>Garden & Outdoor <span className={style.mobile_view_down_arrow}><i className="fa-solid fa-angle-down"></i></span> </div></li>
+          <li className={style.amazon_items_li}><div className={style.felx_arrow}>Storage & Organisation <span className={style.mobile_view_down_arrow}><i className="fa-solid fa-angle-down"></i></span></div></li>
         </ul>
       </div>
     </>
