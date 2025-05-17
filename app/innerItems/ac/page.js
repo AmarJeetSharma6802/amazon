4d65651770342726ceffe27ce8dcce6876b7fccb
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-05-17 12:21:31 +0530

    Refactor footer and inner items styles for improved layout and responsiveness

--- app/innerItems/ac/page.js
+++ app/innerItems/ac/page.js
@@ -1,8 +1,8 @@
-"use client"
-import React,{useState} from "react";
+"use client";
+import React, { useState } from "react";
 import style from "../../style/innerItems.module.css";
 function page() {
-   const [price, setPrice] = useState(31000);
+  const [price, setPrice] = useState(31000);
   return (
     <>
       <div className={style.flex_content}>
@@ -18,26 +18,52 @@ function page() {
             </li>
           </ul>
           <p className={style.flex_content_left_para}>Customer Reviews</p>
-          <ul className={style.flex_content_left_ul}>
-            <li className={style.flex_content_left_ul_li}> <span><i className="fa-solid fa-star"></i></span></li>
-            <li className={style.flex_content_left_ul_li}> <span><i className="fa-solid fa-star"></i></span></li>
-            <li className={style.flex_content_left_ul_li}> <span><i className="fa-solid fa-star"></i></span></li>
-            <li className={style.flex_content_left_ul_li}> <span><i className="fa-solid fa-star"></i></span></li>
-            <li className={style.flex_content_left_ul_li}> <span><i className="fa-regular fa-star"></i></span></li>
-           
+          <ul
+            className={`${style.flex_content_left_ul} ${style.flex_content_left_ul_flex}`}
+          >
+            <li className={style.flex_content_left_ul_li}>
+              {" "}
+              <span className={style.star_span}>
+                <i className="fa-solid fa-star"></i>
+              </span>
+            </li>
+            <li className={style.flex_content_left_ul_li}>
+              {" "}
+              <span className={style.star_span}>
+                <i className="fa-solid fa-star"></i>
+              </span>
+            </li>
+            <li className={style.flex_content_left_ul_li}>
+              {" "}
+              <span className={style.star_span}>
+                <i className="fa-solid fa-star"></i>
+              </span>
+            </li>
+            <li className={style.flex_content_left_ul_li}>
+              {" "}
+              <span className={style.star_span}>
+                <i className="fa-solid fa-star"></i>
+              </span>
+            </li>
+            <li className={style.flex_content_left_ul_li}>
+              {" "}
+              <span className={style.star_span}>
+                <i className="fa-regular fa-star"></i>
+              </span>
+            </li>
           </ul>
           <p className={style.flex_content_left_para}>Item Condition</p>
-          <p>Selected Price: ₹{price}</p>
+          <p className={style.price_para}>Selected Price: ₹{price}</p>
           <p>₹31,000 – ₹52,300+</p>
-           <input
-                type="range"
-                min="31000"
-                max="52300"
-                value={price}
-                onChange={(e) => setPrice(e.target.value)}
-                style={{ width: '100px' }}
-            />
-<p className={style.flex_content_left_para}>Pay On Delivery</p>
+          <input
+            type="range"
+            min="31000"
+            max="52300"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            className={style.input_price}
+          />
+          <p className={style.flex_content_left_para}>Pay On Delivery</p>
           <ul className={style.flex_content_left_ul}>
             <p>Discount</p>
             <li className={style.flex_content_left_ul_li}>10% Off or more</li>
@@ -46,11 +72,69 @@ function page() {
             <li className={style.flex_content_left_ul_li}>50% Off or more</li>
             <li className={style.flex_content_left_ul_li}>60% Off or more</li>
             <li className={style.flex_content_left_ul_li}>70% Off or more</li>
-           
           </ul>
 
+          <p className={style.flex_content_left_para}>Seller</p>
+          <ul className={style.flex_content_left_ul}>
+            <p>Discount</p>
+            <li
+              className={`${style.flex_content_left_ul_li} ${style.flex_content_left_ul_li_inputBox}`}
+            >
+              <span className={style.input_box_sapn}>
+                <input type="checkbox" />
+              </span>{" "}
+              Appliances Seller
+            </li>
+            <li
+              className={`${style.flex_content_left_ul_li} ${style.flex_content_left_ul_li_inputBox}`}
+            >
+              <span className={style.input_box_sapn}>
+                <input type="checkbox" />
+              </span>{" "}
+              25% Off or more
+            </li>
+            <li
+              className={`${style.flex_content_left_ul_li} ${style.flex_content_left_ul_li_inputBox}`}
+            >
+              <span className={style.input_box_sapn}>
+                <input type="checkbox" />
+              </span>
+              35% Off or more
+            </li>
+            <li
+              className={`${style.flex_content_left_ul_li} ${style.flex_content_left_ul_li_inputBox}`}
+            >
+              <span className={style.input_box_sapn}>
+                <input type="checkbox" />
+              </span>
+              50% Off or more
+            </li>
+            <li
+              className={`${style.flex_content_left_ul_li} ${style.flex_content_left_ul_li_inputBox}`}
+            >
+              <span className={style.input_box_sapn}>
+                <input type="checkbox" />
+              </span>
+              60% Off or more
+            </li>
+            <li
+              className={`${style.flex_content_left_ul_li} ${style.flex_content_left_ul_li_inputBox}`}
+            >
+              <span className={style.input_box_sapn}>
+                <input type="checkbox" />
+              </span>
+              70% Off or more
+            </li>
+          </ul>
         </div>
-        <div className={style.flex_content_right}></div>
+        <div className={style.flex_content_right}>
+          <img src="https://m.media-amazon.com/images/I/61JyEPdw3UL._AC_UL320_.jpg"/>
+          <img src="https://m.media-amazon.com/images/I/61JyEPdw3UL._AC_UL320_.jpg"/>
+          <img src="https://m.media-amazon.com/images/I/61JyEPdw3UL._AC_UL320_.jpg"/>
+          <img src="https://m.media-amazon.com/images/I/61JyEPdw3UL._AC_UL320_.jpg"/>
+          <img src="https://m.media-amazon.com/images/I/61JyEPdw3UL._AC_UL320_.jpg"/>
+          <img src="https://m.media-amazon.com/images/I/61JyEPdw3UL._AC_UL320_.jpg"/>
+        </div>
       </div>
     </>
   );
