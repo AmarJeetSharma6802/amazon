2a12b071fd2bf5980f29a744ec5c60d9b4a4196c
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-08-09 14:39:01 +0530

    `Removed Clarity script and added Footer component to RootLayout`

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,15 +34,6 @@ export default function RootLayout({
     >
       <html lang="en">
         <head>
-          {`
-      <script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "sro1vxt8va");
-</script>
-      `}
 
           {/* <link rel="icon" href="" type="image/png" /> */}
           <link
@@ -59,6 +50,7 @@ export default function RootLayout({
         <body className={`${geistSans.variable} ${geistMono.variable}`}>
           <Header />
           {children}
+          
           <Footer />
         </body>
       </html>
